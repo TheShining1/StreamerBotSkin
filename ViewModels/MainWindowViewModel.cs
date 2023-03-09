f68414a2c99353ca1b4a812a2251eb33771a5d2e
TheShiningOne <theshining1ne@gmail.com>
2023-03-09 16:56:46 +0100

    init

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Themes.Fluent;
+using ReactiveUI;
+
+namespace StreamerBotSkin.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public ICommand ChangeThemeCommand { get; }
+
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace StreamerBotSkin.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
