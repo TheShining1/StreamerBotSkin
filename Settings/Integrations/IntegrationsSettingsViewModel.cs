4a287050b13eba920cbdf70a74a88e49b4b848f7
TheShiningOne <theshining1ne@gmail.com>
2024-01-23 16:58:14 +0100

    integration placeholder

--- Settings/Integrations/IntegrationsSettingsViewModel.cs
+++ Settings/Integrations/IntegrationsSettingsViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace StreamerBotSkin.ViewModels
+﻿using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using DynamicData.Binding;
+public class TabItemViewModel
 {
-    internal class IntegrationsSettingsViewModel : ViewModelBase
+  public string Header { get; set; }
+  public string Content { get; set; }
+}
+
+namespace StreamerBotSkin.ViewModels
+{
+  public class IntegrationsSettingsViewModel : ViewModelBase
     {
+      public ObservableCollection<TabItemViewModel> TabItems { get; } = populate();
+
+      static ObservableCollection<TabItemViewModel> populate()
+      {
+        var TabItems = new ObservableCollection<TabItemViewModel>();
+        for (int i = 1; i <= 20; i++)
+        {
+            TabItems.Add(new TabItemViewModel
+            {
+                Header = $"Tab {i}",
+                Content = $"Content for Tab {i}"
+            }
+            );
+        }
+        return TabItems;
+      }
     }
 }
