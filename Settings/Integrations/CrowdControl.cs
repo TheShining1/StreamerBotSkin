3bd798ccf40ee15ffcc3998a9456c65731536a0b
TheShiningOne <theshining1ne@gmail.com>
2024-01-30 15:44:12 +0100

    adds integrations tabs

---
+++ Settings/Integrations/CrowdControl.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StreamerBotSkin;
+
+public partial class CrowdControl : UserControl
+{
+    public CrowdControl()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Settings/Integrations/HypeRate.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace StreamerBotSkin;
+
+public partial class HypeRate : UserControl
+{
+    public HypeRate()
+    {
+        InitializeComponent();
+    }
+}
