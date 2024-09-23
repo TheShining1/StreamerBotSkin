3e9814e6e8e1486719f3ef95db42ae9ae166a32d
TheShiningOne <theshining1ne@gmail.com>
2024-09-23 14:21:49 +0200

    add controls project

---
+++ StreamerBotControls/MainBar/MainBarMenu.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace StreamerBotControls
+{
+  public partial class MainBarMenu : UserControl
+  {
+    public MainBarMenu()
+    {
+      InitializeComponent();
+    }
+  }
+}
